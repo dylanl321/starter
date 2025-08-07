48266594b79244626cb373c25adc4f060c8c46be
Dylan Lewis <dylan@dlewis.me>
2025-08-07 10:27:57 -0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio'
+    }
+  },
   modules: [
     '@nuxt/content',
   ],
